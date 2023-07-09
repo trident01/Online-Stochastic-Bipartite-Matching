c66e3fdb5793ef41d25758ae73235ab4807ee5c2
Tristan Pollner <tristan.pollner@gmail.com>
2023-07-09 12:21:07 -0700

    Update edgeWeightedFinalBound.py

--- edgeWeightedFinalBound.py
+++ edgeWeightedFinalBound.py
@@ -6,9 +6,9 @@ mp.dps = 25
 
 def g(x,t):
 	return (1 - (1-t)*(x/(1-t) - math.floor(x/(1-t))) ) * t ** math.floor(x/(1-t))
-	
+
 def bound(x,epsilon,delta,t):
-	return 1-(1-g(x*(1-epsilon),t*(1-epsilon)))*math.exp(-(1+delta)*(1-x))
+	return 1- g(x*(1-epsilon),t*(1-epsilon))*math.exp(-(1+delta)*(1-x))
 
 N = 1000000
 epsilon = mpf(0.11)
